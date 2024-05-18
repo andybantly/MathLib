a07c86cbedef11e8a7caecf0fce7759be6171c94
Andrew S. Bantly <andybantly@hotmail.com>
2024-05-18 12:19:58 -0500

    ASB - 5/18/2024 - Allow input to include math operation, like 5 * 3, or Ten - 2, or three + eleven

--- MathLib.cpp
+++ MathLib.cpp
@@ -99,14 +99,34 @@ static void test()
 		delete* it;
 }
 
+void Split(const string& strInput, vector<string>& vstrTokens, const char cFind)
+{
+	if (strInput.empty())
+		return;
+
+	std::string strToken;
+	size_t istart = 0, ipos;
+	do
+	{
+		ipos = strInput.find(cFind, istart);
+		if (ipos == string::npos)
+			vstrTokens.push_back(strInput.substr(istart));
+		else
+		{
+			strToken = strInput.substr(istart, ipos - istart);
+			vstrTokens.push_back(strToken);
+			istart = ipos + 1;
+		}
+	} while (ipos != string::npos);
+}
+
 int main()
 {
 	string strInput, strResult;
 	bool bAgain = true;
-	CNumber LastNumber("0");
 	do
 	{
-		cout << "Enter a number or 'test' for verification or 'quit' to exit: ";
+		cout << "Enter a number, set of numbers separated by arithmetic, or 'test' for verification or 'quit' to exit. " << endl << "Input: ";
 
 		strInput.clear();
 		char c;
@@ -116,18 +136,45 @@ int main()
 		{
 			try
 			{
-				CNumber Number(strInput);
-				const string& strNumber = Number.GetNumber();
-				const string& strPhrase = Number.GetPhrase();
-				const string& strBinary = Number.GetBinary();
-				string strBase10;
-				Number.ToBase10(strBinary, strBase10);
-				cout << strNumber << " = " << strPhrase << " = " << strBinary << " = " << strBase10 << endl;
-				cout << "+" << endl << LastNumber.GetNumber() << endl;
-				CNumber Sum = Number + LastNumber;
-				Sum.ToBase10(Sum.GetBinary(), strBase10);
-				cout << Sum.GetNumber() << " = " << Sum.GetPhrase() << " = " << Sum.GetBinary() << " = " << strBase10 << endl;
-				LastNumber = Number;
+				vector<string> vNumbers;
+				Split(strInput, vNumbers, ' ');
+				if (vNumbers.size() == 1)
+				{
+					CNumber Number(strInput);
+					const string& strNumber = Number.GetNumber();
+					const string& strPhrase = Number.GetPhrase();
+					const string& strBinary = Number.GetBinary();
+					string strBase10;
+					Number.ToBase10(strBinary, strBase10);
+					cout << strNumber << " = " << strPhrase << " = " << strBinary << " = " << strBase10 << endl;
+				}
+				else if (vNumbers.size() == 3)
+				{
+					CNumber Number(vNumbers[0]);
+					CNumber Number2(vNumbers[2]);
+
+					string strBase10;
+					Number.ToBase10(Number.GetBinary(), strBase10);
+					cout << Number.GetNumber() << " = " << Number.GetPhrase() << " = " << Number.GetBinary() << " = " << strBase10 << endl;
+					cout << vNumbers[1] << endl;
+					Number2.ToBase10(Number2.GetBinary(), strBase10);
+					cout << Number2.GetNumber() << " = " << Number2.GetPhrase() << " = " << Number2.GetBinary() << " = " << strBase10 << endl;
+					CNumber Sum;
+					if (vNumbers[1] == "+")
+						Sum = Number + Number2;
+					else if (vNumbers[1] == "-")
+						Sum = Number - Number2;
+					else if (vNumbers[1] == "*")
+						Sum = Number * Number2;
+					else if (vNumbers[1] == "/")
+						Sum = Number / Number2;
+					else if (vNumbers[1] == "%")
+						Sum = Number % Number2;
+					Sum.ToBase10(Sum.GetBinary(), strBase10);
+					cout << Sum.GetNumber() << " = " << Sum.GetPhrase() << " = " << Sum.GetBinary() << " = " << strBase10 << endl;
+				}
+				else
+					cout << "Invalid input." << endl;
 			}
 			catch (std::exception& e)
 			{
