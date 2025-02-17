eeffa505c9670734af5756a63f28419a64fd8d85
Andrew S. Bantly <andybantly@hotmail.com>
2025-02-17 12:47:31 -0600

    ASB - 2/17/2025 - Clean up basic UI

--- MathLib.cpp
+++ MathLib.cpp
@@ -6,16 +6,13 @@
 
 #pragma warning(disable:6385)
 
-using namespace std; // get rid of this
-
 int main()
 {
-	CNumber::Init();
-	string strInput, strResult;
+	std::string strInput, strResult;
 	bool bAgain = true;
 	do
 	{
-		std::cout << "Enter a number, set of numbers separated by arithmetic or comparison operations (+,-,*,/,%, <, >, <=, >=, =)" << endl << "Enter 'test' for verification or 'quit' to exit." << endl << "Examples: 2147483647, 12 * 11 + 12, three + 4 - two, 9 / 3 + 7 * 10, 10 % 3, 2 < 3" << endl << "Input: ";
+		std::cout << "Enter a number, set of numbers separated by arithmetic or comparison operations (+,-,*,/,%, <, >, <=, >=, =)" << std::endl << "Enter 'test' for verification or 'quit' to exit." << std::endl << "Examples: 2147483647, 12 * 11 + 12, three + 4 - two, 9 / 3 + 7 * 10, 10 % 3, 2 < 3" << std::endl << "Input: ";
 
 		strInput.clear();
 		char c;
@@ -25,15 +22,15 @@ int main()
 		{
 			try
 			{
-				vector<string> vNumbers;
-				vector<string> vNumbers2;
+				std::vector<std::string> vNumbers;
+				std::vector<std::string> vNumbers2;
 				Split(strInput, vNumbers, ' ');
 				if (vNumbers.size() > 3)
 				{
-					string strConcatNumber;
-					for (vector<string>::iterator it = vNumbers.begin(); it != vNumbers.end(); ++it)
+					std::string strConcatNumber;
+					for (std::vector<std::string>::iterator it = vNumbers.begin(); it != vNumbers.end(); ++it)
 					{
-						if (*it == "+" || *it == "-" || *it == "*" || *it == "/" || *it == "%")
+						if (*it == "+" || *it == "-" || *it == "*" || *it == "/" || *it == "%" || *it == ",")
 						{
 							vNumbers2.push_back(strConcatNumber);
 							vNumbers2.push_back(*it);
@@ -58,118 +55,87 @@ int main()
 
 				if (vNumbers.size() == 1)
 				{
-					CNumber Number1(strInput);
 					Number N(strInput);
-					const string& strPhrase = Number1.GetPhrase();
-					const string& strBinary = Number1.GetBinary();
-					string strBase10;
-					Number1.ToBase10(strBinary, strBase10);
-					std::cout << Number1 << " " << strBinary << endl;
-					std::cout << N << " " << N.ToBinary() << endl;
-					std::cout << endl;
+					std::cout << N << " " << N.ToBinary() << std::endl;
 				}
 				else if (vNumbers.size() >= 3)
 				{
 					do
 					{
-						CNumber Number1(vNumbers[0]);
-						CNumber Number2(vNumbers[2]);
-						CNumber Sum;
-
 						Number N1(vNumbers[0]);
 						Number N2(vNumbers[2]);
 						Number S;
 
-						string strBase10;
-						Number1.ToBase10(Number1.GetBinary(), strBase10);
-						std::cout << Number1 << " " << Number1.GetBinary() << endl;
-
-						std::cout << vNumbers[1] << endl;
-
-						Number2.ToBase10(Number2.GetBinary(), strBase10);
-						std::cout << Number2 << " " << Number2.GetBinary() << endl;
-
-						bool bCMP = false, bCMP2 = false;
+						bool bCMP = false;
 						if (vNumbers[1] == "+")
 						{
-							Sum = Number1 + Number2;
 							S = N1 + N2;
 						}
 						else if (vNumbers[1] == "-")
 						{
-							Sum = Number1 - Number2;
 							S = N1 - N2;
 						}
 						else if (vNumbers[1] == "*")
 						{
-							Sum = Number1 * Number2;
 							S = N1 * N2;
 						}
 						else if (vNumbers[1] == "/")
 						{
-							Sum = Number1 / Number2;
 							S = N1 / N2;
 						}
 						else if (vNumbers[1] == "%")
 						{
-							Sum = Number1 % Number2;
 							S = N1 % N2;
+						}
+						else if (vNumbers[1] == ",")
+						{
+							S = (N1 , N2);
 						}
 						else if (vNumbers[1] == "<")
 						{
-							bCMP = Number1 < Number2;
-							bCMP2 = N1 < N2;
+							bCMP = N1 < N2;
 						}
 						else if (vNumbers[1] == ">")
 						{
-							bCMP = Number1 > Number2;
-							bCMP2 = N1 > N2;
+							bCMP = N1 > N2;
 						}
 						else if (vNumbers[1] == "=")
 						{
-							bCMP = Number1 == Number2;
-							bCMP2 = N1 == N2;
+							bCMP = N1 == N2;
 						}
 						else if (vNumbers[1] == "<=")
 						{
-							bCMP = Number1 <= Number2;
-							bCMP2 = N1 <= N2;
+							bCMP = N1 <= N2;
 						}
 						else if (vNumbers[1] == ">=")
 						{
-							bCMP = Number1 >= Number2;
-							bCMP2 = N1 >= N2;
+							bCMP = N1 >= N2;
 						}
 
-						if (vNumbers[1] == "+" || vNumbers[1] == "-" || vNumbers[1] == "*" || vNumbers[1] == "/" || vNumbers[1] == "%")
+						if (vNumbers[1] == "+" || vNumbers[1] == "-" || vNumbers[1] == "*" || vNumbers[1] == "/" || vNumbers[1] == "%" || vNumbers[1] == ",")
 						{
-							Sum.ToBase10(Sum.GetBinary(), strBase10);
-							std::cout << Sum.GetNumber() << " = " << Sum.GetBinary() << endl;
-							std::cout << S.ToDisplay() << " = " << S.ToBinary() << endl;
+							std::cout << N1 << " " << vNumbers[1] << " " << N2 << " = " << S.ToDisplay() << std::endl << S.ToBinary() << std::endl;
 						}
 						else
 						{
-							Sum = bCMP ? "1" : "0";
-							std::cout << Number1.GetNumber() << " " + vNumbers[1] + " " << Number2.GetNumber() << " = " << (bCMP ? "TRUE" : "FALSE") << endl;
-
-							S = bCMP2 ? "1" : "0";
-							std::cout << N1 << " " + vNumbers[1] + " " << N2 << " = " << (bCMP2 ? "TRUE" : "FALSE") << endl;
+							S = bCMP ? "1" : "0";
+							std::cout << N1 << " " + vNumbers[1] + " " << N2 << " = " << (bCMP ? "TRUE" : "FALSE") << std::endl;
 						}
 
 						vNumbers2.clear();
-						vNumbers2.push_back(Sum.GetNumber());
-						for (std::vector<string>::iterator vit = vNumbers.begin() + 3; vit != vNumbers.end(); ++vit)
+						vNumbers2.push_back(S.ToDisplay());
+						for (std::vector<std::string>::iterator vit = vNumbers.begin() + 3; vit != vNumbers.end(); ++vit)
 							vNumbers2.push_back(*vit);
 						vNumbers = vNumbers2;
-						std::cout << endl;
+						std::cout << std::endl;
 					} while (vNumbers.size() > 1);
 				}
 				else
-					std::cout << "Invalid input." << endl;
+					std::cout << "Invalid input." << std::endl;
 			}
 			catch (std::exception& e)
 			{
-				std::cout << e.what() << endl;
+				std::cout << e.what() << std::endl;
 			}
 		}
 		else
@@ -187,7 +153,7 @@ static void ttest(unsigned long long ullb, unsigned long long ulle)
 {
 	CNumber Number;
 
-	string s, sr, sv;
+	std::string s, sr, sv;
 	unsigned long long ull = ullb;
 	double dp = 0.0, dt = double(ulle - ullb);
 	clock_t start, finish;
@@ -195,33 +161,33 @@ static void ttest(unsigned long long ullb, unsigned long long ulle)
 	start = clock();
 
 	{
-		lock_guard<mutex> guard(g_io_mutex);
-		std::cout << "thread from: " << ullb << " to " << ulle << endl;
+		std::lock_guard<std::mutex> guard(g_io_mutex);
+		std::cout << "thread from: " << ullb << " to " << ulle << std::endl;
 	}
 
 	do
 	{
-		s = to_string(ull++);
+		s = std::to_string(ull++);
 		if (Number.Expand(s, sr) != 0)
 		{
-			lock_guard<mutex> guard(g_io_mutex);
-			std::cout << s << " did not expand" << endl;
+			std::lock_guard<std::mutex> guard(g_io_mutex);
+			std::cout << s << " did not expand" << std::endl;
 			break;
 		}
 		else
 		{
 			if (Number.Contract(sr, sv) != 0)
 			{
-				lock_guard<mutex> guard(g_io_mutex);
-				std::cout << sr << " did not contract" << endl;
+				std::lock_guard<std::mutex> guard(g_io_mutex);
+				std::cout << sr << " did not contract" << std::endl;
 				break;
 			}
 			else
 			{
 				if (sv != s)
 				{
-					lock_guard<mutex> guard(g_io_mutex);
-					std::cout << s << " did not expand and contract" << endl;
+					std::lock_guard<std::mutex> guard(g_io_mutex);
+					std::cout << s << " did not expand and contract" << std::endl;
 					break;
 				}
 			}
@@ -229,30 +195,30 @@ static void ttest(unsigned long long ullb, unsigned long long ulle)
 
 		if (ull % 100000 == 0)
 		{
-			lock_guard<mutex> guard(g_io_mutex);
+			std::lock_guard<std::mutex> guard(g_io_mutex);
 			finish = clock();
 			double d = (double)(finish - start) / CLOCKS_PER_SEC;
 			if (d > 60.0)
 			{
 				dp = (double(ull - ullb) / dt) * 100.0;
-				std::cout << "Expanded/Contracted " << (ull - ullb) << " out of " << (ulle - ullb) << ", value " << s << " at " << dp << "%" << endl;
+				std::cout << "Expanded/Contracted " << (ull - ullb) << " out of " << (ulle - ullb) << ", value " << s << " at " << dp << "%" << std::endl;
 				start = clock();
 			}
 		}
 	} while (ull <= ulle && ull != 0);
 
-	lock_guard<mutex> guard(g_io_mutex);
-	std::cout << "thread finished" << endl;
+	std::lock_guard<std::mutex> guard(g_io_mutex);
+	std::cout << "thread finished" << std::endl;
 }
 
 static void test()
 {
 	CDuration Duration("Test Expansion/Contraction:");
 
-	unsigned long long numt = thread::hardware_concurrency();
+	unsigned long long numt = std::thread::hardware_concurrency();
 	unsigned long long dtpt = INT_MAX / numt;
 
-	vector<thread*> vptp;
+	std::vector<std::thread*> vptp;
 	unsigned long long ullb, ulle;
 	for (unsigned long long it = 0; it < numt; it++)
 	{
@@ -262,18 +228,18 @@ static void test()
 		else
 			ulle = INT_MAX;
 
-		thread* ptp = new thread(ttest, ullb, ulle);
+		std::thread* ptp = new std::thread(ttest, ullb, ulle);
 		vptp.push_back(ptp);
 	}
 
-	for (vector<thread*>::iterator it = vptp.begin(); it != vptp.end(); ++it)
+	for (std::vector<std::thread*>::iterator it = vptp.begin(); it != vptp.end(); ++it)
 		(*it)->join();
 
-	for (vector<thread*>::iterator it = vptp.begin(); it != vptp.end(); ++it)
+	for (std::vector<std::thread*>::iterator it = vptp.begin(); it != vptp.end(); ++it)
 		delete *it;
 }
 
-void Split(const string& strInput, vector<string>& vstrTokens, const char cFind)
+void Split(const std::string& strInput, std::vector<std::string>& vstrTokens, const char cFind)
 {
 	if (strInput.empty())
 		return;
@@ -283,7 +249,7 @@ void Split(const string& strInput, vector<string>& vstrTokens, const char cFind)
 	do
 	{
 		ipos = strInput.find(cFind, istart);
-		if (ipos == string::npos)
+		if (ipos == std::string::npos)
 			vstrTokens.push_back(strInput.substr(istart));
 		else
 		{
@@ -291,6 +257,5 @@ void Split(const string& strInput, vector<string>& vstrTokens, const char cFind)
 			vstrTokens.push_back(strToken);
 			istart = ipos + 1;
 		}
-	} while (ipos != string::npos);
+	} while (ipos != std::string::npos);
 }
-
